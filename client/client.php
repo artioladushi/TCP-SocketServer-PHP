6e4a253441fc86e262aa9520619a10ec0e26a2be
diartselmani <diart.selmani@student.uni-pr.edu>
2026-04-17 20:06:49 +0200

    shtova pak kod
    shtova nje pjese te kodit te serveri dhe ia fillova me client, po ashtu kishte nje gabim tek mesazhi e vendosa te ai .txt

--- client/client.php
+++ client/client.php
@@ -1,2 +1,17 @@
 Ky është një file tjetër për testim.
 Vetëm admin mund të shkruajë në file të serverit.
+<?php
+
+$SERVER_IP = '10.180.52.41';   // change this to real server IP for other devices
+$SERVER_PORT = 5000;
+
+$client = stream_socket_client("tcp://{$SERVER_IP}:{$SERVER_PORT}", $errno, $errstr, 30);
+
+if (!$client) {
+    die("Connection failed: $errstr ($errno)\n");
+}
+
+echo "Connected to server {$SERVER_IP}:{$SERVER_PORT}\n";
+echo "Write commands and press Enter.\n";
+echo "Example: LOGIN admin admin123\n";
+echo "Type EXIT to close client.\n\n";
